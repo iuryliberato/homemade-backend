727fc9407e6bab0b2dbb52d1dd4fbaeeaa6ef663
iuryliberato <iuryliberato@hotmail.com>
2024-03-26 13:53:28 -0300

    added .env file

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,30 +1,30 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import cors from 'cors'
+
 import 'dotenv/config'
 import serverless from "serverless-http"
 
 import logger from '../../lib/logger.js'
-import { connectDB } from '../../db/helpers.js'
+import { startServer } from '../../index.js'
 import router from '../../config/router.js'
 
 const api = express()
 
 api.use(bodyParser.json())
-api.use(cors())
+
 api.use('/', logger)
 api.use('/api', router)
 
 
 
-function startServer() {
+function starterServer() {
   try {
-    connectDB()
+    startServer()
   } catch (err) {
     console.log('❌❌ Error: ', err, err.message)
   }
 }
 
-startServer()
+starterServer()
 
 export const handler = serverless(api)
