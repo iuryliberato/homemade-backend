d095910fdf39939f4abba6c8462eba558e8b2f97
iuryliberato <iuryliberato@hotmail.com>
2024-03-28 08:31:20 -0300

    added export to startServer

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ import mongoose from 'mongoose'
 import router from './config/router.js'
 import 'dotenv/config'
 
-// import path, { dirname } from 'path'
-// import { fileURLToPath } from 'url'
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
 
 const app = express()
 
 
-const startServer = async () => {
+export const startServer = async () => {
   try {
     console.log(process.env.dbURI)
     await mongoose.connect(process.env.DB_URI)
